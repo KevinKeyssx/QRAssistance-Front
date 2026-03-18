b2e4744211b3e1c4e68199783d74ae0112aa10d3
kevinkeyssx <keyss1700@gmail.com>
2026-03-18 00:28:25 -0300

    Background implemented & favicon updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 
-    import favicon                          from '$lib/assets/favicon.svg';
 	import { initDarkMode, setDarkMode }    from '$lib/utils/darkMode';
     import Header                           from '$lib/components/page/Header.svelte';
     import Footer                           from '$lib/components/page/Footer.svelte';
@@ -24,14 +23,22 @@
 	}
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href="/favicon.ico" type="image/x-icon" /></svelte:head>
 
 <div class="relative min-h-dvh flex flex-col bg-white dark:bg-gray-900 transition-colors duration-300">
-	<Header {darkMode} onToggle={handleToggle} />
+	<img
+        src     = "/fullLightRays.png"
+        alt     = "background"
+        class   = "fixed top-0 left-0 w-full h-full object-cover object-center pointer-events-none opacity-40 dark:opacity-10 select-none z-0"
+    />
 
-    <main class="flex-1 flex flex-col items-center justify-center">
-        {@render children()}
-    </main>
+	<div class="relative z-10 flex flex-col min-h-dvh">
+		<Header {darkMode} onToggle={handleToggle} />
 
-    <Footer />
+		<main class="flex-1 flex flex-col items-center justify-center">
+			{@render children()}
+		</main>
+
+		<Footer />
+	</div>
 </div>
