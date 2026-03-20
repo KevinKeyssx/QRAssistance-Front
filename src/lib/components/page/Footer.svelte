77d5143fb92d18f3a48dcf439950cd77bc6f0b47
kevinkeyssx <keyss1700@gmail.com>
2026-03-19 22:12:43 -0300

    Styles updated

--- src/lib/components/page/Footer.svelte
+++ src/lib/components/page/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="w-full mt-auto relative flex flex-col items-center justify-center border-t border-gray-200 dark:border-gray-800 bg-transparent py-1 sm:py-10 border-b-4" >
+<footer class="w-full mt-auto relative flex flex-col items-center justify-center border-t border-gray-200 dark:border-gray-800 bg-transparent py-1 sm:py-3">
     <div class="relative z-10 flex shrink-0 items-center justify-center w-32 sm:w-40 md:w-48">
 		<img
             src     = "/spa.png"
