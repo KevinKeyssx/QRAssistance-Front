87ce234f21b7ec132f1e3fd11ce2a5464aafefb1
kevinkeyssx <keyss1700@gmail.com>
2026-03-20 20:50:50 -0300

    Icons implemented

--- src/lib/components/shared/Check.svelte
+++ src/lib/components/shared/Check.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 
-	interface Props {
+    import CautionIcon      from '$lib/icons/CautionIcon.svelte';
+    import CheckIcon        from '$lib/icons/CheckIcon.svelte';
+
+
+    interface Props {
 		id          : string;
 		checked     : boolean;
 		title       : string;
@@ -31,9 +35,7 @@
 			{!checked && error ? 'border-red-500 dark:border-red-400' : ''}
 		">
 			{#if checked}
-				<svg class="w-3 h-3 text-white dark:text-gray-900" fill="currentColor" viewBox="0 0 20 20">
-					<path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-				</svg>
+                <CheckIcon />
 			{/if}
 		</div>
 	</div>
@@ -60,9 +62,8 @@
 
 		{#if error && !checked}
 			<p class="mt-3 text-xs text-red-600 dark:text-red-400 flex items-center gap-1.5 font-medium">
-				<svg class="w-4 h-4 shrink-0" fill="currentColor" viewBox="0 0 20 20">
-					<path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
-				</svg>
+                <CautionIcon size={16} />
+
 				{error}
 			</p>
 		{/if}
