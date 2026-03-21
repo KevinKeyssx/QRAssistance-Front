f4733ae6bccb18768b16ccc70516a0832d932e78
kevinkeyssx <keyss1700@gmail.com>
2026-03-21 19:09:28 -0300

    QR icon size props implemented

--- src/lib/icons/QRIcon.svelte
+++ src/lib/icons/QRIcon.svelte
@@ -1,7 +1,11 @@
+<script>
+    let { size = 22 } = $props();
+</script>
+
 <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="22"
-    height="22"
+    width={size}
+    height={size}
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
