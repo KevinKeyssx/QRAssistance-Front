6958cfa9f9f0041a8868b7da2a66f502628128f1
kevinkeyssx <keyss1700@gmail.com>
2026-04-04 23:47:08 -0400

    ROUTER added

---
+++ src/lib/utils/apis.ts
@@ -0,0 +1,10 @@
+export const ROUTER = {
+    INTERNAL: {
+        VALIDATE_ASSISTANCE : 'validate-assistance',
+        REGISTER_MEMBER     : 'register-member',
+        SEARCH_MEMBER       : 'search-member',
+    },
+    SURVEY: {
+        CREATE : 'survey/create',
+    }
+}
