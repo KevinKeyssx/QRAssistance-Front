5ec645204da936bb43578dc443549dff40e8cb5a
kevinkeyssx <keyss1700@gmail.com>
2026-05-09 00:06:08 -0400

    REGISTER_VISITOR implemented

--- src/lib/utils/apis.ts
+++ src/lib/utils/apis.ts
@@ -2,6 +2,7 @@ export const ROUTER = {
     INTERNAL: {
         VALIDATE_ASSISTANCE : 'validate-assistance',
         REGISTER_MEMBER     : 'register-member',
+        REGISTER_VISITOR    : 'register-visitor',
         SEARCH_MEMBER       : 'search-member',
     },
     SURVEY: {
